007f22c774ff26b4e1264a6adbb65cfa14771f15
Jeremy Reynolds <jreynolds323@gmail.com>
2014-05-23 12:36:07 -0500

    comment added.

--- animationDelay.sh
+++ animationDelay.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
+# creates file called animation.txt containing all animation times.
 
 echo "Please enter number of images"
 read input
-echo "What would you like your file to be called?"
-read fileName
-touch $fileName
+touch animation.txt
+
 count=1
 while [ $count -le $input ]
 do
 	echo "What is image $count's delay? "
 	read input1
 	echo "$input1,"  >> $fileName 
+		
 	count=$(($count + 1))
 	#echo $count
 done
-
-#x=1
-#while [ $x -le 5 ]
-#do
-#	echo "Welcome $x times"
-#	x=$(($x + 1))
-#done
