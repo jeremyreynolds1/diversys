8c25c20025c45a080901ab0b5c72419df006ddd3
Jeremy Reynolds <jreynolds323@gmail.com>
2014-05-23 09:33:27 -0500

    animationDelay.sh to help with creating array for custom animation times.

--- animationDelay.sh
+++ animationDelay.sh
@@ -5,10 +5,19 @@ read input
 echo "What would you like your file to be called?"
 read fileName
 touch $fileName
-counter=0
-
-while [ $counter -lt $input ] 
+count=1
+while [ $count -le $input ]
 do
-	echo The counter is $counter
-	let counter=counter+1
+	echo "What is image $count's delay? "
+	read input1
+	echo $input1 >> $fileName
+	count=$(($count + 1))
+	#echo $count
 done
+
+#x=1
+#while [ $x -le 5 ]
+#do
+#	echo "Welcome $x times"
+#	x=$(($x + 1))
+#done
